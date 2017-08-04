86b26f1a5d6e2406d6ee6858a82d3a06c8c2fbda
Rob Lim <robert.m.lim@gmail.com>
2017-08-04 01:13:06 -0700

    Create Map class

--- week1_day5_hw/diy_adts.rb
+++ week1_day5_hw/diy_adts.rb
@@ -31,6 +31,43 @@ class Queue
   end
 
   def show
-    p @queue
+    @queue
   end
+end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, val)
+    if lookup(key)
+      remove(key)
+      @map << [key, val]
+    else
+      @map << [key, val]
+    end
+  end
+
+  def lookup(key)
+    @map.each { |key_value_pair| return key_value_pair[1] if key_value_pair[0] == key }
+    nil
+  end
+
+  def remove(key)
+    return nil unless lookup(key)
+    pair = nil
+    @map.each_with_index do |key_value_pair, idx|
+      if key_value_pair[0] == key
+        pair = @map.delete_at(idx)
+        break
+      end
+    end
+    return pair[1]
+  end
+
+  def show
+    @map
+  end
+
 end
